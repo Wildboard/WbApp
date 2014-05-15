05c4cffebe8aa326ba5f6944a0a7ee4a069420c7
Gregory Golberg <grisha@alum.mit.edu>
2014-05-15 02:24:04 +0000

    demo!!!!

--- www/js/views/GetAd.js
+++ www/js/views/GetAd.js
@@ -1,5 +1,5 @@
 (function(window) {
-  console.log("top: Get Ad");
+    console.log("Entering GetAd view.");
 
     function wbAlert(s) {
       navigator.notification.alert(s, function(){});
@@ -14,68 +14,155 @@
 
   app.GetAd = function() {
     $(".pageGetAd").css("display", "block");
-    console.log("entered app.GetAd");
-    //if (!app.GetAd.initiated) {
-      app.GetAd.init();
-      //}
+    app.GetAd.init();
     return app.GetAd.Content;
   }
 
   app.GetAd.Content = $(".pageGetAd");
-  app.GetAd.Options = {}
+  app.GetAd.Options = {};
+  app.GetAd.Options.socket = null;
   app.GetAd.initiated = false;
+
   app.GetAd.init = function() {
-    var inProgressDialog = null;
+
     app.GetAd.initiated = true;
+
     app.GetAd.Options = {};
-    var sess = Math.floor((Math.random()*1000000)+1);
+
+    function fetchSavedFlyers() {
+	var flyersStr = window.localStorage.getItem("flyers");
+	var flyersArr = [];
+	if (flyersStr) {
+	    flyersArr = flyersStr.split(",");
+	}  
+	return flyersArr;
+    }
+
+    function refreshSavedFlyers() {
+	var flyersArr = fetchSavedFlyers();
+	console.log("Fetched " + flyersArr.length + " saved flyers");
+	for (var flyerId in flyersArr) {
+	    console.log("Got " + flyerId);
+	    try {
+		var flyerJsonStr = window.localStorage.getItem("flyerJson_"+flyerId);
+		var flyerJson = eval(flyerJsonStr);
+		var imgSrc = flyerJson.mediaArea[0];
+		var title = flyerJson.titleArea.title;
+		var subtitle = "";
+		for (var sub in flyerJson.titleArea.subtitle) { 
+		    subtitle += sub + "<br/>\n";
+		}
+		var desc = flyerJson.bodyArea.description;
+		var contact = "";
+		for (var con in flyerJson.contactArea) {
+		    contact += con.text + "<br/>\n";
+		    // TODO type
+		}
+		var html = "<div id=\"divFlyer" + flyerId + "\">";
+		html += "<h1>" + title + "</h1>";
+		html += "<h2>" + subtitle + "</h2>";
+		html += contact + "<br/>";
+		html += "<img height=\"40\" width=\"30\" src=\"" + imgSrc + "\"></img>";
+		html += "</div>";
+		$('#savedFlyersOuterDiv').html(html);
+	    } catch (ex) {
+		// do nothing
+	    }
+	}
+    }
+
+
     
     $(".pageGetAd").css("display", "block");
     app.GetAd.Content = $(".pageGetAd");
+    $('#serverMsg').html("");
     
+    // TODO????
+    $('#idGetAdArrowUp').css("display", "none");
 
-    var socket = io.connect('http://ads.wildboard.net:8888');
-   
     function onConnect() {
-      socket.emit('phoneHi', { deviceId : device.uuid});
+	// Step 9.
+	app.GetAd.Options.socket.emit('phoneHi', { deviceId : device.uuid});
     }
-
 
     function onDisconnect() {
       $('#serverMsg').html("Disconnected");
+      app.GetAd.Options.socket = null;
     }
 
+    // Step 13.
     function onOops(data) {
-      navigator.notification.alert(data.msg, function(){});
-      console.log(data.msg);
+	var msg = data.msg;
+        wbAlert(msg);
+	$('#serverMsg').html(msg);
     }      
+
+    function disconnect() {
+	console.log("Disconnecting.");
+	if (app.GetAd.Options.socket) {
+	    try {
+		app.GetAd.Options.socket.disconnect();
+	    } catch (ex) {
+		console.log("Error disconnecting: " + ex);
+	    }
+	}
+	$('#idGetAdArrowUp').css("display", "none");
+	app.GetAd.Options.socket = null;
+    }
 
     function onWelcome(data) {
       if (!data || !data.length) {
-	wbAlert("No boards available");
-	socket.disconnect()
-	return;
+	  // Should not happen!
+	  wbAlert("No boards available");
+	  return;
       }
+      // Step 14 in progress
       wbAlert("Connecting to " + data[0]);
-      socket.emit('phoneConnectTo', {boardName : data[0]});
+      app.GetAd.Options.socket.emit('phoneConnectTo', {boardName : data[0]});
     }
     
     function onGetAd(data) {
       var flyerId = data.flyerId;
       var flyerHtml = data.flyerHtml;
-      console.log("Received " + data.flyerId);
-      $('#serverMsg').html(flyerHtml);
+      var flyerJson = data.flyerJson;
+      console.log("Received " + data.flyerId + "; saving.");
+      // Step 27.
+      
+      window.localStorage.setItem("flyerJson_" + flyerId, flyerJson);
+      var savedFlyersArr = fetchSavedFlyers();
+      var savedFlyersStr = savedFlyersArr.join();
+      window.localStorage.setItem("flyers", savedFlyersStr);
+
+      var imgSrc = flyerJson.mediaArea[0];
+      imgSrc = imgSrc.replace("localhost/images","ads.wildboard.net/images/demo");
+
+      console.log("Image: " + imgSrc);
+      var html = "<img height=\"40\" width=\"30\" src=\"" + imgSrc + "\"></img>";
+      $('#savedFlyersOuterDiv').html(html);
+
+      //refreshSavedFlyers();
+
+      // Step 28.
+      disconnect();
     }
     
     
     function onYourColorIs(data) {
 	var myColor = data.color;
+	// Step 20. 
 	$('#serverMsg').html('<font color="' + 
 			     myColor + 
 			     '">Drag the flyer to the ' +
 			     myColor +
 			     ' area on the screen to get it on your phone.' +
 			     '</font>');
+	// http://css-tricks.com/snippets/css/css-triangle/
+	$('#idGetAdArrowUp').css("display", "block");
+	$('#idGetAdArrowUp').css("width", "0");
+	$('#idGetAdArrowUp').css("height", "0");
+	$('#idGetAdArrowUp').css("border-left", "200px solid transparent");
+	$('#idGetAdArrowUp').css("border-right", "200px solid transparent");
+	$('#idGetAdArrowUp').css("border-bottom", "40px solid " +myColor);
     }
 	
     
@@ -83,24 +170,27 @@
     // hi (client->server)
     // oops (server->client)
     // welcome (server->client)
+    disconnect();
+    if (!app.GetAd.Options.socket) {
+	try {
+	    // Step 7.
+	    console.log("Trying to connect");
+	    app.GetAd.Options.socket = io.connect('http://ads.wildboard.net:8888');
+	    console.log("Connected: " + app.GetAd.Options.socket.id);
+	} catch (ex) {
+	    // Step 8.
+	    wbAlert("Cannot establish connection");
+	    $('#serverMsg').html("No connection.");
+	    return;
+	}
+    } 
 
-    socket.on('connect', onConnect);
-    socket.on('toPhoneOops', onOops);
-    socket.on('toPhoneWelcome', onWelcome);
-    socket.on('toPhoneYourColorIs', onYourColorIs);
-    socket.on('toPhoneGetAd', onGetAd);
-    socket.on('disconnect', onDisconnect);
+    app.GetAd.Options.socket.on('connect', onConnect);
+    app.GetAd.Options.socket.on('toPhoneOops', onOops);
+    app.GetAd.Options.socket.on('toPhoneWelcome', onWelcome);
+    app.GetAd.Options.socket.on('toPhoneYourColorIs', onYourColorIs);
+    app.GetAd.Options.socket.on('toPhoneGetAd', onGetAd);
+    app.GetAd.Options.socket.on('disconnect', onDisconnect);
     
-    //function(){
-    //			socket.on('event', function(data){});
-    //			
-    //		      });
-    
-    $("#base64image").unbind("touchstart");
-    $("#base64image").bind("touchstart", function(event) {
-			     var imgLarge = document.getElementById('imgLargeDiv');
-			     imgLarge.style.display = 'block';
-			     imgLarge.style.visibility = 'visible';
-			   });
   }
 })(window);
